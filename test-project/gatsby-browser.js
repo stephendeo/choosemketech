f31f12ea7b15aa6306411187cc8f206b47a207c8
stephen <smahadeo@nvisia.com>
2021-07-01 00:34:56 -0500

    added memberstack

--- test-project/gatsby-browser.js
+++ test-project/gatsby-browser.js
@@ -3,3 +3,16 @@ import "./src/assets/style.css"
 // import "./src/assets/style-rtl.css"
 import "./src/assets/print.css"
 import "./src/assets/style-theme.css"
+
+const memberStackScript = url => {
+  const script = document.createElement("script")
+  script.src = url
+  script.setAttribute('data-memberstack-id','b3a96bc0b7fb2bfeb080a0ad91819570')
+  document.body.appendChild(script)
+}
+
+export const onClientEntry = () => {
+  window.onload = () => {
+    memberStackScript("https://api.memberstack.io/static/memberstack.js?custom")
+  }
+}
